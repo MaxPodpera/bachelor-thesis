9b8758575f3e00fb321bb68e18ff3dfff6601788
Max Podpera <max@podpera.com>
2021-11-16 22:23:30 +0100

    small changes

--- node/main.py
+++ node/main.py
@@ -2,16 +2,12 @@ import time
 
 from Messenger import Messenger
 
-class Test:
-
-    def __init__(self):
-        # TODO register file interface
-        # init lora device
-        # on file or network action take file or network action respectively
-        m = Messenger()
-        time.sleep(2)
-        m.start()
-        m.send({"data": "hello there", "recipient": {"pid": "asdsd", "at": "45:32:21:ae:bc"}})
-
 if __name__ == '__main__':
-    t = Test()
+    # TODO register file interface
+    # init lora device
+    # on file or network action take file or network action respectively
+    m = Messenger()
+    time.sleep(2)
+    m.start()
+
+    m.send({"data": "hello there", "recipient": {"pid": "asdsd", "at": "45:32:21:ae:bc"}})
