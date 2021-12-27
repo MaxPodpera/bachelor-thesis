869e356f1d0bef9e75e2b9af1e841c17b1fe234f
Max Podpera <max@podpera.com>
2021-12-27 12:23:24 +0100

    small changes

--- node/main.py
+++ node/main.py
@@ -16,13 +16,13 @@ if __name__ == '__main__':
     m.start()
     time.sleep(2)
     #
-    #if m.node_id == "42376f7500df44e985e8f7255bcfa0f7":
-    #    time.sleep(10)
     # Hard coded message TODO interaction with FS to load messages.
     message = Message()
     message.data = "Hello there this is an amazing message that is very long so it needs more than one package" \
-                   "to be sent over completely. It will be printed once everything was received"#. This requires some" \
-                   #"more characters though."
+                   "to be sent over completely. It will be printed once everything was received. This requires some" \
+                   "more characters though. But to completely and thorough test this i will now attempt to create a" \
+                   "message that is long enough to be sent as three messages. This way the later two should be" \
+                   " received. This line is the last one i need for this."
     message.recipient = "42376f7500df44e985e8f7255bcfa0f7"
     message.pid = 11111
     message.sender_pid = 22222
