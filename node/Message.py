3275595f63bd4dbd04458c6d6f3beedcfdd940d5
Max Podpera <max@podpera.com>
2022-02-21 16:46:51 +0200

    small changes

--- node/Message.py
+++ node/Message.py
@@ -6,7 +6,7 @@ from util.Utilities import read_config_file
 """
 Message class. Contains definition of the Messages and functionality to convert messages to bytes and the other way around.
 """
-print(read_config_file("message"))
+print(read_config_file("uuid_file"))
 print(read_config_file("message.meta"))
 print(read_config_file("message.meta.length_id"))
 print(int(read_config_file("message.meta.length_id")))
