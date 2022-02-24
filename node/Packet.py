0b313ce8651d7f3a5ae332359d067fa2f70af0b4
Max Podpera <max@podpera.com>
2022-02-24 13:54:00 +0200

    small changes

--- node/Packet.py
+++ node/Packet.py
@@ -18,7 +18,7 @@ address_broadcast: str = read_config_file("message.broadcast_address")
 class Packet:
     #         to  from  id   flags
     headers: (int, int, int, int) = (255, 255, 0, 0)
-    b: bytearray = bytearray()
+    b: bytearray = None
 
     def __init__(self, headers: (int, int, int, int), data_bytes: bytearray):
         self.headers = headers
@@ -107,6 +107,7 @@ def from_message(message: Message) -> [Packet]:
 
         meta = b + seq_str
         data = data_bytes[:length_max_data]
+        print(meta + data)
         result.append(Packet(headers, meta + data))
         
         data_bytes = data_bytes[len(data):]
