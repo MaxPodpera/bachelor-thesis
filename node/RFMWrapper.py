8862ec8328c4d5f0e6b86de5700217c7255a4d08
Max Podpera <max@podpera.com>
2022-03-10 12:25:49 +0200

    small changes

--- node/RFMWrapper.py
+++ node/RFMWrapper.py
@@ -58,7 +58,7 @@ class RFMWrapper:
         d = self._rfm95.receive(with_header=True)
         if d is None:
             return None
-        
+        print("\n\n", d , "\n\n")
         return to_message(d)
 
 
