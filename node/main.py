0c69894059ee01e51ab037487f419d7cba124daf
Max Podpera <max@podpera.com>
2022-03-13 12:43:40 +0200

    small changes

--- node/main.py
+++ node/main.py
@@ -22,10 +22,10 @@ if __name__ == '__main__':
     # Hard coded message TODO interaction with FS to load messages.
     message = Message()
     message.data = "Hello there this is an amazing message that is very long so it needs more than one package" \
-                   #"to be sent over completely. It will be printed once everything was received. This requires some" \
-                   #"more characters though. But to completely and thorough test this i will now attempt to create a" \
-                   #"message that is long enough to be sent as three messages. This way the later two should be" \
-                   #" received. This line is the last one i need for this."
+                   "to be sent over completely. It will be printed once everything was received. This requires some" \
+                   "more characters though. But to completely and thorough test this i will now attempt to create a" \
+                   "message that is long enough to be sent as three messages. This way the later two should be" \
+                   " received. This line is the last one i need for this."
 
     message.recipient = "2b679c67277302db4ca0ae3fcbad51d3" #  "42376f7500df44e985e8f7255bcfa0f7"
     message.pid = 11111
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     
     message1 = Message()
     message1.data = "Hello again. You guessed it. This is another amazing message that will be transmitted." \
-                    #"The purpose of this message is to see if transmission fails because of split messages" \
-                    #"or just because its too fast or something. But whatever! I will find out soon enough"
+                    "The purpose of this message is to see if transmission fails because of split messages" \
+                    "or just because its too fast or something. But whatever! I will find out soon enough"
     message1.recipient = message.recipient
     message1.pid = 11111
     message1.sender_pid = 22222
