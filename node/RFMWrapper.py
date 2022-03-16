6c02d4707d2d4932f42db878bf9b8dd64c645cb8
Max Podpera <max@podpera.com>
2022-03-16 11:48:13 +0200

    small changes

--- node/RFMWrapper.py
+++ node/RFMWrapper.py
@@ -62,5 +62,6 @@ class RFMWrapper:
         d = self._rfm95.receive(with_header=True)
         if d is None:
             return None
-        logging.info("Received package: ", to_message(d))
+        logging.info("Received package: ")
+        print(to_message(d))
         return to_message(d)
