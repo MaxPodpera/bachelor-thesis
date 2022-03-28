694899cf1d2c6f768c36afdf54a56f538bba578b
Max Podpera <max@podpera.com>
2022-03-28 20:23:31 +0300

    small changes

--- node/Messenger.py
+++ node/Messenger.py
@@ -18,6 +18,7 @@ class Messenger:
     node_id: str = None
 
     def __init__(self):
+        logging.critical("test")
         self._rfm95 = RFMWrapper()
         self.node_id = read_uuid_file(read_config_file("uuid_file"))
         self._organiser = MessageOrganiser(self.node_id)
