c131be86a5b357a520baccd8caeca634815646ff
Max Podpera <max@podpera.com>
2022-03-31 12:31:38 +0300

    small changes

--- src/Message.py
+++ src/Message.py
@@ -35,8 +35,8 @@ def to_message(package: bytes) -> Union[Message, None]:
         bytes_to_convert = package[4:]
 
         # Check data before converting
-        print("\n",package, "\n")
-        valid, package = remove_and_check(package)
+        print("\n", bytes_to_convert, "\n")
+        valid, package = remove_and_check(bytes_to_convert)
         if not valid:
             logging.info("Received invalid package, discarding")
             return None
