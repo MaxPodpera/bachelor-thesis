9834d015747dd6d010553bb3c05c0af5e89f83a8
Max Podpera <max@podpera.com>
2022-03-31 15:04:34 +0300

    small changes

--- src/RFMWrapper.py
+++ src/RFMWrapper.py
@@ -26,6 +26,7 @@ class RFMWrapper:
         baudrate = 1000000
         frequency = 868.0
         self._rfm95 = adafruit_rfm9x.RFM9x(pin_spi, pin_cs, pin_rst, frequency, baudrate=baudrate)
+        self._rfm95.reset()
         # see documentation for meaning
         self._rfm95.ack_delay = .1
         self._rfm95.ack_retries = 2
