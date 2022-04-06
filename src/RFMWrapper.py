bf950ca8f926f84e497f456038e1bc6e257928a5
Max Podpera <max@podpera.com>
2022-04-06 12:51:32 +0300

    small changes

--- src/RFMWrapper.py
+++ src/RFMWrapper.py
@@ -26,11 +26,12 @@ class RFMWrapper:
         baudrate = 1000000
         frequency = 868.0
         self._rfm95 = adafruit_rfm9x.RFM9x(pin_spi, pin_cs, pin_rst, frequency, baudrate=baudrate)
+        self._rfm95.reset()
         # see documentation for meaning
-        self._rfm95.ack_delay = .1
-        self._rfm95.ack_retries = 2
+        # self._rfm95.ack_delay = .1
+        # self._rfm95.ack_retries = 2
         # Wait time before resending
-        self._rfm95.ack_wait = .3
+        # self._rfm95.ack_wait = .3
         self._rfm95.node = 255  # so all packages will be received
         logging.info("Setup of module complete")
 
