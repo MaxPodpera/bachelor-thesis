5e92bd44cd3e867e3bba1414d8282f1c8b34a878
Max Podpera <max@podpera.com>
2022-06-08 20:20:14 +0200

    testcase_2_10sec_3pck_100msg

--- src/main.py
+++ src/main.py
@@ -23,9 +23,9 @@ if __name__ == '__main__':
             msg.pid = 11111
             msg.recipient = "17109be4e4b711ec8fea0242ac120002"
             msg.sender = "42376f7500df44e985e8f7255bcfa0f7"
-            msg.data = str(datetime.now()) + str(i) + ("A" * 90) + str(i)
+            msg.data = str(datetime.now()) + str(i)
             m.send(msg)
-            time.sleep(1)
+            time.sleep(10)
 
         # time.sleep(3600 * 2)
         #
@@ -58,9 +58,9 @@ if __name__ == '__main__':
             msg.pid = 11111
             msg.recipient = "17109be4e4b711ec8fea0242ac120002"
             msg.sender = "2b679c67277302db4ca0ae3fcbad51d3"
-            msg.data = str(datetime.now()) + str(i) + ("D" * 90) + str(i)
+            msg.data = str(datetime.now()) + str(i)
             m.send(msg)
-            time.sleep(1)
+            time.sleep(10)
         #
         # time.sleep(3600 * 2)
         #
