bfcc2804d4600792da0e3e3c24c8060720fe0a91
Max Podpera <max@podpera.com>
2022-06-08 22:35:14 +0200

    testcase_2_10sec_3pck_100msg

--- src/main.py
+++ src/main.py
@@ -29,40 +29,6 @@ if __name__ == '__main__':
 
         time.sleep(3600 * 3)
 
-        for i in range(0, 100):
-            msg = Message()
-            msg.sender_pid = 00000
-            msg.pid = 11111
-            msg.recipient = "17109be4e4b711ec8fea0242ac120002"
-            msg.sender = "42376f7500df44e985e8f7255bcfa0f7"
-            msg.data = str(datetime.now()) + str(i) + ("A" * 90) + str(i) + ("A" * 80)
-            m.send(msg)
-            time.sleep(10)
-
-        time.sleep(3600 * 3)
-
-        for i in range(0, 100):
-            msg = Message()
-            msg.sender_pid = 00000
-            msg.pid = 11111
-            msg.recipient = "17109be4e4b711ec8fea0242ac120002"
-            msg.sender = "42376f7500df44e985e8f7255bcfa0f7"
-            msg.data = str(datetime.now()) + str(i) + ("B" * 90) + str(i) + ("B" * 80)
-            m.send(msg)
-            time.sleep(5)
-
-        time.sleep(3600 * 3)
-
-        for i in range(0, 100):
-            msg = Message()
-            msg.sender_pid = 00000
-            msg.pid = 11111
-            msg.recipient = "17109be4e4b711ec8fea0242ac120002"
-            msg.sender = "42376f7500df44e985e8f7255bcfa0f7"
-            msg.data = str(datetime.now()) + str(i) + ("C" * 90) + str(i) + ("C" * 80)
-            m.send(msg)
-            time.sleep(1)
-
     if m.node_id == "2b679c67277302db4ca0ae3fcbad51d3":
         for i in range(0, 100):
             msg = Message()
@@ -86,7 +52,7 @@ if __name__ == '__main__':
             m.send(msg)
             time.sleep(1)
 
-        time.sleep(3600 * 3)
+        time.sleep(3600 * 4)
 
         for i in range(0, 100):
             msg = Message()
@@ -98,7 +64,7 @@ if __name__ == '__main__':
             m.send(msg)
             time.sleep(5)
 
-        time.sleep(3600 * 3)
+        time.sleep(3600 * 4)
 
         for i in range(0, 100):
             msg = Message()
