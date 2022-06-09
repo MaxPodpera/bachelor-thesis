f83df2720c6436bbf4d117fcd0208a32ea927d02
Max Podpera <max@podpera.com>
2022-06-09 11:24:46 +0200

    testcase_2_10sec_3pck_100msg

--- src/main.py
+++ src/main.py
@@ -16,19 +16,6 @@ if __name__ == '__main__':
     t = threading.Thread(target=m.run)
     t.start()
 
-    if m.node_id == "42376f7500df44e985e8f7255bcfa0f7":
-        for i in range(0, 100):
-            msg = Message()
-            msg.sender_pid = 00000
-            msg.pid = 11111
-            msg.recipient = "17109be4e4b711ec8fea0242ac120002"
-            msg.sender = "42376f7500df44e985e8f7255bcfa0f7"
-            msg.data = str(datetime.now()) + str(i)
-            m.send(msg)
-            time.sleep(1)
-
-       # time.sleep(3600 * 3)
-
     if m.node_id == "2b679c67277302db4ca0ae3fcbad51d3":
         for i in range(0, 100):
             msg = Message()
@@ -36,9 +23,9 @@ if __name__ == '__main__':
             msg.pid = 11111
             msg.recipient = "17109be4e4b711ec8fea0242ac120002"
             msg.sender = "2b679c67277302db4ca0ae3fcbad51d3"
-            msg.data = str(datetime.now()) + str(i) #  + ("F" * 90) + str(i) + ("F" * 80)
+            msg.data = str(datetime.now()) + str(i) + ("F" * 90) + str(i) + ("F" * 80)
             m.send(msg)
-            time.sleep(1)
+            time.sleep(10)
 
         # time.sleep(3600 * 3)
 
